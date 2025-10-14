c91d3b183392c6020c815bfa8ecf19d475638980
Édgar Julián González Sierra <ejgonzalez@javeriana.edu.co>
2025-10-13 19:03:22 -0500

    CD

--- deploy-llm.sh
+++ deploy-llm.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-git pull
 
-sudo docker build -t llm-service .
+docker build -t llm-service .
 
-sudo docker stop llm-service
+docker stop llm-service
 
-sudo docker rm llm-service
+docker rm llm-service
 
-sudo docker run -d --name llm-service -p 8001:8001 --network argy-network llm-service
+docker run -d --name llm-service -p 8001:8001 --network argy-network llm-service
 

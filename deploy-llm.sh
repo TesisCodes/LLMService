f3af12147fd254dfdc06645a85c991bdfaf7c3ff
Édgar Julián González Sierra <ejgonzalez@javeriana.edu.co>
2025-10-23 21:51:05 -0500

    port solved

--- deploy-llm.sh
+++ deploy-llm.sh
@@ -6,5 +6,5 @@ sudo docker stop llm-service
 
 sudo docker rm llm-service
 
-sudo docker run -d --name llm-service -p 8001:8001 --network argy-network llm-service
+sudo docker run -d --name llm-service -p 8001 --network argy-network llm-service
 
